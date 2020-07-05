e8e8bbb39ba4d736a93804d4c23c0b2c8780ec00
Nam Ho Koh <lucaskoh.lk@gmail.com>
2020-07-04 22:16:03 -0700

    Created RollerAgent script.

---
+++ Assets/Scripts/RollerAgent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class RollerAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
