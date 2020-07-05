4d25d1ab63e5d5e23e35396bade502c8a142ce10
Nam Ho Koh <lucaskoh.lk@gmail.com>
2020-07-04 22:25:58 -0700

    Added OnEpisodeBegin() method.

--- Assets/Scripts/RollerAgent.cs
+++ Assets/Scripts/RollerAgent.cs
@@ -6,9 +6,23 @@ using Unity.MLAgents.Sensors;
 
 public class RollerAgent : Agent
 {
-    // Start is called before the first frame update
+    Rigidbody rBody;
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody>();
+    }
+    // To move the targetX (Cube), we nee to reference to to its Transform. 
+    public Transform TargetX;
+    public override void OnEpisodeBegin() {
+        if (this.transform.localPosition.y < 0) {
+            // If the Agent fell, zero its momemtum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+
+        // Move the targetX to a new spot
+        TargetX.localPosition = new Vector3(Random.value * 8 - 4, 0.5f,
+                                            Random.value * 8 - 4);
     }
 }
